0aff6cd77f5ea8ed1e53b626d10d5a5bb5ccfde9
David Queen <dsqjr@msn.com>
2021-10-13 23:22:41 -0400

    failed test

--- test/main.spec.js
+++ test/main.spec.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect;
 describe('main program works',()=>{
 
     it('can add two numbers',()=>{
-        expect(main(4,4)).to.equal(8);
+        expect(main(4,4)).to.equal(7);
 
     })
 });
