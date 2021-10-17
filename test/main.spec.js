8cc33763c199a0eba5304e89d834e0257a7dc738
David Queen <dsqjr@msn.com>
2021-10-16 22:32:01 -0400

    fixedt test

--- test/main.spec.js
+++ test/main.spec.js
@@ -9,7 +9,7 @@ describe('main program works',()=>{
 
     });
     it('can multiply two numbers',()=>{
-        expect(main.multiply(4,4)).to.equal(15);
+        expect(main.multiply(4,4)).to.equal(16);
 
     })
 });
